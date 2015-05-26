6456efa4ffefb193e124e69986bb83d895f40f04
Nathan Andrade <nathan_ita@hotmail.com>
2015-05-26 19:27:07 -0300

    Updated design

--- app/src/main/java/com/impostocerto/areas/ProjectActivity.java
+++ app/src/main/java/com/impostocerto/areas/ProjectActivity.java
@@ -1,9 +1,15 @@
 package com.impostocerto.areas;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.impostocerto.login.R;
 
@@ -14,6 +20,7 @@ public class ProjectActivity extends ActionBarActivity {
 
     private Toolbar toolbar;                              // Declaring the Toolbar Object
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +36,54 @@ public class ProjectActivity extends ActionBarActivity {
             subtitle = extras.getString("SUBAREA");
         }
 
-        if (title.equals("Educação"))
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+
+        if (title.equals("Educação")) {
             toolbar.setBackgroundColor(getResources().getColor(R.color.color_educacao));
-        if (title.equals("Saúde"))
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.color_educacao));
+        }if (title.equals("Saúde")) {
             toolbar.setBackgroundColor(getResources().getColor(R.color.color_saude));
-        if (title.equals("Saneamento"))
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.color_saude));
+        }
+        if (title.equals("Saneamento")){
             toolbar.setBackgroundColor(getResources().getColor(R.color.color_saneamento));
-        if (title.equals("Segurança"))
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.color_saneamento));
+        }if (title.equals("Segurança")) {
             toolbar.setBackgroundColor(getResources().getColor(R.color.color_seguranca));
-        if (title.equals("Transporte"))
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.color_seguranca));
+        }if (title.equals("Transporte")) {
             toolbar.setBackgroundColor(getResources().getColor(R.color.color_transporte));
-        if (title.equals("Urbanização"))
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.color_transporte));
+        }if (title.equals("Urbanização")) {
             toolbar.setBackgroundColor(getResources().getColor(R.color.color_urbanizacao));
-        if (title.equals("Entretenimento"))
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.color_urbanizacao));
+        }if (title.equals("Entretenimento")) {
             toolbar.setBackgroundColor(getResources().getColor(R.color.color_entretenimento));
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.color_entretenimento));
+        }
 
         toolbar.setTitle(title);
         toolbar.setSubtitle(subtitle);
@@ -51,6 +92,12 @@ public class ProjectActivity extends ActionBarActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        ListView list_projetos = (ListView) findViewById(R.id.list_subareas);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item_project, R.id.project_title, values);
+
+        list_projetos.setAdapter(adapter);
     }
 
     @Override
